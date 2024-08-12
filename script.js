5d31949e60f41f3985cbe32fab6a2eadcdbb3a92
alemanshi <134016563+alemanshi@users.noreply.github.com>
2024-08-11 22:37:44 -0300

    primera subida

---
+++ script.js
@@ -0,0 +1,28 @@
+let carrito = [];
+let carrito = 0;
+
+
+function agregarAlCarrito (producto,cantidad,precio) {
+    cantidad = parseInt(cantidad),
+
+    if (cantidad <= 0) {
+        alert ('la cantidad debe ser amyor que 0');
+    return;
+}
+
+function actualizarCarrito() {
+    return;
+}
+
+funtion LimpiarCarrito () {
+    return;
+}
+
+
+funtion EliminarDelCarrito () {
+    return;
+}
+
+
+const numeroWhatsApp = +5491111779481
+const mensaje = 'Hola, me gustaría realizar una compra en tu tienda de los productos detallados debajo:'
